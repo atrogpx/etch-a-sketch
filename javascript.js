e172b0e817d24a14a89eb6f2a3214259d231914b
Atro <103967835+atrogpx@users.noreply.github.com>
2023-08-06 15:39:05 +0200

    change div color when hovered over

--- javascript.js
+++ javascript.js
@@ -3,5 +3,7 @@ const container = document.querySelector('#container');
 for (i = 0; i < 256; i++) {
     const gridElement = document.createElement('div');
     container.appendChild(gridElement);
-    gridElement.classList.add('grid-elements');
+    gridElement.id = 'grid-elements';
+    gridElement.addEventListener('mouseover', () => gridElement.setAttribute('style', 'background: pink;'));
+    gridElement.addEventListener('mouseout', () => gridElement.setAttribute('style', 'background: aqua;'));
 }
